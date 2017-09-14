d5b51768be5133ea4d612b92f61edb1f54756f0a
Laura Mahoney <lauramahoney@me.com>
2017-09-13 20:39:39 -0700

    hamming distance between two numbers and merging two binary trees

---
+++ hamming_distance.py
@@ -0,0 +1,12 @@
+"""Find the hamming distance between two numbers """
+
+
+class Solution(object):
+    def hammingDistance(self, x, y):
+        """
+        :type x: int
+        :type y: int
+        :rtype: int
+        """
+        
+        return bin(x^y).count('1')
